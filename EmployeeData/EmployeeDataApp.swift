3703e5e21be2091a867cb11a0fe181a04a0c8c63
Sushant kumar <susKumar@mafcarrefour.com>
2022-06-03 11:49:17 +0530

    Initial Commit

---
+++ EmployeeData/EmployeeDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeDataApp.swift
+//  EmployeeData
+//
+//  Created by Sushant Kumar on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
